1fabe6b4d6db0d0de0fbe4591ed387d09c0435a4
deepak0898 <94215644+deepak0898@users.noreply.github.com>
2021-11-23 22:59:41 +0530

    Update Bus reservation system_operation.h

--- 3_Implementation/inc/Bus reservation system_operation.h
+++ 3_Implementation/inc/Bus reservation system_operation.h
@@ -1,1 +1,25 @@
+#ifndef _BUS_RESERVATION_SYSTEM_OPERATIONS_H_
+#define _BUS_RESERVATION_SYSTEM_OPERATIONS_H_
+void bus();
+void name_number();
+void booking();
+void read_name();
+void status();
+void status_1(int trno);
+void cancle();
 
+void bus();
+
+void booking();
+
+
+void name_number();
+
+
+void read_name(int trno);
+
+void status();
+void status_1(int trno);
+void cancle();
+void login();
+#endif
